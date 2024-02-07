96af5c67d3267e549d01286cbcb50f719c232ab9
deb-schmiele <debora.schmiele@artit.com.br>
2024-02-07 17:05:26 -0300

    home-page and navi-bar component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePage } from './components/home-page/home-page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomePage},
+  {path: '', component: HomePage},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomePage } from './components/home-page/home-page';
+import { NaviBar } from './components/commons/navi-bar/navi-bar.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePage,
+    NaviBar,
   ],
   imports: [
     BrowserModule,
